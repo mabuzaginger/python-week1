6f8e096566df907b376823c21274edd42177bf89
mabuzaginger <mabuzaeros@gmail.com>
2024-08-14 08:58:40 +0200

    Initial commit

---
+++ practice.py
@@ -0,0 +1,39 @@
+# Let's store a favourite number
+#favouriteNumber = 88
+#print(f"My favourite number is {favouriteNumber}")
+
+#String variable
+#greeting = "Hello, PLP"
+#print(greeting)
+
+#Boolen
+#is_python_fun = True
+#print(f"Is Python fun ? {is_python_fun}")
+
+#Contorl Flow: if, elif, else
+#temperature = 30
+#if temperature > 25:
+#     print("It's hot outside! Wear shorts.")
+# elif temperature > 15:
+#     print("It's warm. Mybe try a light jacket.")
+# else:
+#     print("Be warned! It's cold here, please bundle up!")
+
+# For and While loops
+# for i in  range(5):
+#     print(f"This is a for loop interation {i}")
+    
+    #While loop
+# countdown = 5
+# while countdown > 0:
+#     print(f"countdown: {countdown}")
+#     countdown -= 1
+# print("Blast off!")
+
+#Define a function
+def greet(name):
+    return f"Hello, {name}"
+#Apply the function
+print(greet("mpumi"))
+print(greet("elain"))
+    
